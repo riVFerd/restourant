0c7f8deae1a8197ce4d497a0d1a3487db2bb9cb4
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-03-19 21:20:46 +0700

    finish hw week 11

---
+++ app.py
@@ -0,0 +1,30 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+
+url = "https://www.spacex.com/launches/"
+
+# using requests + BeautifulSoup
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# req = requests.get(url, headers=headers)
+# soup = BeautifulSoup(req.text, 'html.parser')
+#
+# launches = soup.select('#items > div.item')
+# for launch in launches:
+#     print(launch.select_one('.date').text)
+#     print(launch.select_one('.label').text)
+
+# using selenium + BeautifulSoup
+driver = webdriver.Chrome('./chromedriver')
+driver.get(url)
+sleep(5)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+launches = soup.select('#items > div.item')
+for launch in launches:
+    print(launch.select_one('.date').text)
+    print(launch.select_one('.label').text)
