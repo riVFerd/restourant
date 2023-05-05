ce91b79e16427e3277ddb249257363e3f2fa53de
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-05-05 23:31:47 +0700

    finish hw week 14

--- app.py
+++ app.py
@@ -1,42 +1,58 @@
-import bs4
-import requests
-from bs4 import BeautifulSoup
-from flask import render_template, Flask
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.chrome.service import Service
-
-url = "https://www.spacex.com/launches/"
-
-# using requests + BeautifulSoup
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# req = requests.get(url, headers=headers)
-# soup = BeautifulSoup(req.text, 'html.parser')
-#
-# launches = soup.select('#items > div.item')
-# for launch in launches:
-#     print(launch.select_one('.date').text)
-#     print(launch.select_one('.label').text)
-
-# using selenium + BeautifulSoup
-# driver = webdriver.Chrome('./chromedriver')
-# driver.get(url)
-# sleep(5)
-# html = driver.page_source
-# soup = BeautifulSoup(html, 'html.parser')
-#
-# launches = soup.select('#items > div.item')
-# for launch in launches:
-#     print(launch.select_one('.date').text)
-#     print(launch.select_one('.label').text)
+from flask import (
+    Flask,
+    render_template,
+    request,
+    jsonify,
+    redirect,
+    url_for
+)
+import certifi
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+password = 'test_mongodb'
+cxn_str = f'mongodb+srv://riVFerd:{password}@cluster0.rq9u845.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(cxn_str, tlsCAFile=certifi.where())
+db = client.dbsparta
 
-@app.route('/map')
-def map():
-    return render_template('prac_map.html')
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+@app.route('/restaurants', methods=["GET"])
+def get_restaurants():
+    # This api endpoint should fetch a list of restaurants
+    restaurants = list(db.restaurants.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'restaurants': restaurants})
+
+@app.route('/restaurants/delete', methods=["POST"])
+def delete_restaurants():
+    name = request.form.get('name')
+    # This api endpoint should fetch a list of restaurants
+    db.restaurants.delete_one({'name': name})
+    return jsonify({'result': 'success', 'msg': 'Kamu berhasil menghapus data restaurants!'})
+
+
+@app.route('/restaurants/create_restaurant', methods=["POST"])
+def create_restaurant():
+    name = request.form.get('name')
+    categories = request.form.get('categories')
+    location = request.form.get('location')
+    longitude = request.form.get('longitude')
+    latitude = request.form.get('latitude')
+    # This api endpoint should fetch a list of restaurants
+    doc = {
+        'name': name,
+        'categories': categories,
+        'location': location,
+        'center': [longitude, latitude]
+    }
+    db.restaurants.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': 'Kamu berhasil menambahkan data restaurants!'})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
